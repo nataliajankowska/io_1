b5cb85c633cb920884aaf55a8215488f59c89ccf
Natalia Jankowska <jankowska.natalia.nj@gmail.com>
2024-03-24 11:54:35 +0100

    Dodano trapez

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ bok_b = 7
 print(bok_a*bok_b)
 
 #trójkąt 
-wys = 6
-podst = 6
-print(1/2*(wys*podst))
+h = 6
+a = 6
+print(1/2*(h*a))
 
 # kolo
 r = 6
 print (math.pi*r*r)
 
-
+#trapez
+bok_a = 3
+bok_b = 5
+h = 4
+print((bok_a+bok_b)*h/2)
 
